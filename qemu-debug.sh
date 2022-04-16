cee4cb1694770455f6cdd2fe6a064f7307e04ec0
kingdo <1440852110@qq.com>
2022-04-16 16:29:41 +0800

    添加可用于Clion+gdb+qemu进行远程调试的必要修改

---
+++ qemu-debug.sh
@@ -0,0 +1,8 @@
+qemu-system-x86_64 \
+  -kernel arch/x86_64/boot/bzImage \
+  -nographic \
+  -append "console=ttyS0 nokaslr" \
+  -device virtio-balloon \
+  -initrd ramdisk.img \
+  -m 1024 \
+  -s -S
